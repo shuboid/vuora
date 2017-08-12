91bebde7b35855175e6ce112696d34a573e230c1
shubham-amura <shubham@amuratech.com>
2017-08-12 22:41:22 +0530

    Csrf false

--- app/src/helloworld/helloworld/settings.py
+++ app/src/helloworld/helloworld/settings.py
@@ -106,7 +106,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.10/topics/i18n/
-
+CSRF_COOKIE_SECURE = False
 LANGUAGE_CODE = 'en-us'
 
 TIME_ZONE = 'UTC'
