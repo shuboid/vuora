f3a40b812720b4bcf55dde0eec00d270a3fc408e
shubham-amura <shubham@amuratech.com>
2017-08-12 15:58:53 +0530

    fixed

--- app/src/helloworld/helloworld/views.py
+++ app/src/helloworld/helloworld/views.py
@@ -2,7 +2,7 @@ from django.http import HttpResponse, JsonResponse
 import json
 import requests
 from random import randint
-
+import pdb
 from helloworld import settings
 
 
@@ -12,11 +12,12 @@ def index(request):
 def recieve_call(request):
     print(request)
     fileext = randint(0,10)
+    pdb.set_trace()
     response = "<?xml version='1.0' encoding='UTF-8'?>"\
                 "<Response>"\
                   "<playtext speed='4' quality='best' >Thank you for calling Voura. We are always here to help you </playtext>"\
                   "<playtext speed='4' quality='best'> Hey friend! Leave your question for us we will get back to you soon </playtext>"\
-                   "<record format='wav' silence='3' maxduration='40' >current_user_" +fileext+ "</record>"\
+                   "<record format='wav' silence='3' maxduration='40' >current_user_" +str(fileext)+ "</record>"\
                   "</Response>"
     return HttpResponse(response)
 
